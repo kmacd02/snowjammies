2676dd29abb87a7bde2271ddddaef6d84e24cf9e
Caleb Kim <calebmk21@gmail.com>
2022-12-17 15:25:34 -0800

    deleted audio and sounsystem
    dfgsfdsf

--- Assets/Scripts/Caleb Scripts/MenuFunctions.cs
+++ Assets/Scripts/Caleb Scripts/MenuFunctions.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SoundSystem : MonoBehaviour
+public class MenuFunctions : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
--- Assets/Scripts/Caleb Scripts/UserInterfaceScript.cs
+++ Assets/Scripts/Caleb Scripts/UserInterfaceScript.cs
@@ -2,21 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
+using UnityEngine.SceneManagement;
 
 public class UserInterfaceScript : MonoBehaviour
 {
+    public Button PlayButton;
+    public Button SettingsButton;
+    public Button QuitButton;
+    public Button MenuButton;
 
-    private void onEnable()
+    void Start()
     {
-        VisualElement root = GetComponent<UIDocument>.rootVisualElement;
+        var root = GetComponent<UIDocument>().rootVisualElement;
 
-        Button playButton = root.Q<Button>("PlayButton");
-        Button settingsButton = root.Q<Button>("SettingsButton");
-        Button quitButton = root.Q<Button>("QuitButton");
-        
+        PlayButton = root.Q<Button>("PlayButton");
+        SettingsButton = root.Q<Button>("SettingsButton");
+        QuitButton = root.Q<Button>("QuitButton");
+        MenuButton = root.Q<Button>("MenuButton");
 
+        PlayButton.clicked += PlayButtonPressed;
+        SettingsButton.clicked += SettingsButtonPressed;
     }
 
+    void PlayButtonPressed()
+    {
+        SceneManager.LoadScene("main");
+    }
 
+    void SettingsButtonPressed()
+    {
 
+    }
 }
