f657657aafb90d1da3d1a2a2bf51ea5658728445
bluepaw59 <bluepaw59@gmail.com>
2022-12-16 23:55:36 -0800

    basic game over implemented
    doesn't change based on sprites

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -9,13 +9,20 @@ using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
     public int status; //0 = continue, 1 = win/fail, 2 = neutral
-
+    [SerializeField] GameObject gameoverscreen;
     //TODO: Add array of sprites for gameover screens
 
     // Start is called before the first frame update
     void Start()
     {
         status = 0;
+    }
+
+    public void restartGame()
+    {
+        status = 0;
+        //TODO: reset all locations and whatnot
+        gameoverscreen.SetActive(false);
     }
 
     // Update is called once per frame
@@ -26,8 +33,11 @@ public class GameManager : MonoBehaviour
             case 0: //continue
                 break;
             case 1: //win or fail
+                //TODO: set gameoverscreen sprite/image
+                gameoverscreen.SetActive(true);
                 break;
             case 2: //neutral
+                //gameoverscreen.setActive(true);
                 break;
             default: //error
                 Debug.Log("Invalid game status. Must be 0, 1, 2, or 3.");
