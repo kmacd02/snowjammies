449efcd81a3a05f72e15f25d0719acff3b72de3f
bluepaw59 <bluepaw59@gmail.com>
2022-12-16 21:38:29 -0800

    combining items done

--- Assets/Scripts/Karen scripts/CombiningItems.cs
+++ Assets/Scripts/Karen scripts/CombiningItems.cs
@@ -7,11 +7,6 @@ public class CombiningItems : MonoBehaviour
     [SerializeField] private string item1 = "";
     [SerializeField] private string item2 = "";
 
-    // Start is called before the first frame update
-    // void Start()
-    // {
-        
-    // }
 
 
     //**************************************************
@@ -27,6 +22,7 @@ public class CombiningItems : MonoBehaviour
     //**************************************************
     //adds itemName item to whichever item is free
     //also checks if it is a valid itemName
+    // if its the second item, combines it with the first item
     //**************************************************
     public void addItem(string itemName)
     {
@@ -35,18 +31,34 @@ public class CombiningItems : MonoBehaviour
         {
             Debug.Log("itemName is not a valid item");
         }
-        if(item1 == "")
-        {
-            item1 = itemName;
-        }
         else
         {
-            item2 = itemName;
+            if(item1 == "")
+            {
+                item1 = itemName;
+            }
+            else
+            {
+                item2 = itemName;
+            }
+            if(item1 != "" && item2 != "")
+            {
+                combineItems();
+            }
         }
-        if(item1 != "" && item2 != "")
+    }
+
+
+    //************************************
+    //returns true if item is at workstation
+    //************************************
+    public bool itemIsUsed(string itemName)
+    {
+        if (itemName == item1 || itemName == item2)
         {
-            combineItems();
+            return true;
         }
+        return false;
     }
 
 
@@ -55,6 +67,105 @@ public class CombiningItems : MonoBehaviour
     //**************************************************
     private void combineItems()
     {
+        if (item1 == "blanket" || item2 == "blanket")
+        {
+            if (item1 == "candle" || item2 == "candle")
+            {
+                Debug.Log("F burn the house down");
+            }
+            else if (item1 == "blowtorch" || item2 == "blowtorch")
+            {
+                Debug.Log("F burn the house down");
+            }
+            else if (item1 == "laptop" || item2 == "laptop")
+            {
+                Debug.Log("W");
+            }
+            else if (item1 == "toaster" || item2 == "toaster")
+            {
+                Debug.Log("N");
+            }
+            else if (item1 == "fork" || item2 == "fork")
+            {
+                Debug.Log("N");
+            }
+            else
+            {
+                Debug.Log(item1 + " and " + item2 + " are an invalid combination");
+            }
+        }
+        else if (item1 == "candle" || item2 == "candle")
+        {
+            if (item1 == "blowtorch" || item2 == "blowtorch")
+            {
+                Debug.Log("raise the chicken from the dead");
+            }
+            else if (item1 == "laptop" || item2 == "laptop")
+            {
+                Debug.Log("W");
+            }
+            else if (item1 == "toaster" || item2 == "toaster")
+            {
+                Debug.Log("W");
+            }
+            else if (item1 == "fork" || item2 == "fork")
+            {
+                Debug.Log("N");
+            }
+            else
+            {
+                Debug.Log(item1 + " and " + item2 + " are an invalid combination");
+            }
+        }
+        else if (item1 == "blowtorch" || item2 == "blowtorch")
+        {
+            if (item1 == "laptop" || item2 == "laptop")
+            {
+                Debug.Log("F summons CS bro who tells you heat is bad for laptop performance (rants and you run out of time)");
+            }
+            else if (item1 == "toaster" || item2 == "toaster")
+            {
+                Debug.Log("N toaster melts - burnt toast reference?");
+            }
+            else if (item1 == "fork" || item2 == "fork")
+            {
+                Debug.Log("W");
+            }
+            else
+            {
+                Debug.Log(item1 + " and " + item2 + " are an invalid combination");
+            }
+        }
+        else if (item1 == "laptop" || item2 == "laptop")
+        {
+            if (item1 == "toaster" || item2 == "toaster")
+            {
+                Debug.Log("F Start playing Burnt ToaSt");
+            }
+            else if (item1 == "fork" || item2 == "fork")
+            {
+                Debug.Log("N Stab the screen with the fork. Laptop breaks.");
+            }
+            else
+            {
+                Debug.Log(item1 + " and " + item2 + " are an invalid combination");
+            }
+        }
+        else if (item1 == "toaster" || item2 == "toaster")
+        {
+            if (item1 == "fork" || item2 == "fork")
+            {
+                Debug.Log("F - Atomic bomb - mushroom cloud gif");
+            }
+            else
+            {
+                Debug.Log(item1 + " and " + item2 + " are an invalid combination");
+            }
+        }
+        else
+        {
+            Debug.Log(item1 + " and " + item2 + " are an invalid combination");
+        }
         //Todo: check combinations
         reset();
     }
