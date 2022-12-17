778c8dd566eae03621ebb5a663243275a3c9f236
Kenzie MacDougal <kmacd02@g.ucla.edu>
2022-12-16 22:56:02 -0500

    Update GameManager.cs

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    int status; //0 = continue, 1 = win, 2 = neutral, 3 = fail
+    public int status; //0 = continue, 1 = win, 2 = neutral, 3 = fail
 
     // Start is called before the first frame update
     void Start()
