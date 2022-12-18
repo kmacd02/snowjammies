8a467b904c24034f4b18a6fe38f3f5a2f782eb44
Caleb Kim <calebmk21@gmail.com>
2022-12-17 18:08:48 -0800

    Base UI test

--- Assets/Scripts/Caleb Scripts/UserInterfaceScript.cs
+++ Assets/Scripts/Caleb Scripts/UserInterfaceScript.cs
@@ -8,42 +8,49 @@ using UnityEngine.SceneManagement;
 public class UserInterfaceScript : MonoBehaviour
 {
     public Button PlayButton;
-    public Button SettingsButton;
+    public Button OptionsButton;
     public Button QuitButton;
     public Button MenuButton;
     public Button HowToButton;
 
+    private VisualElement Container;
+
     void Start()
     {
         var root = GetComponent<UIDocument>().rootVisualElement;
+        Container = root.Q<VisualElement>("Container");
+        
+        // Gathering Button Roots
 
         PlayButton = root.Q<Button>("PlayButton");
-        SettingsButton = root.Q<Button>("SettingsButton");
+        OptionsButton = root.Q<Button>("OptionsButton");
         QuitButton = root.Q<Button>("QuitButton");
         MenuButton = root.Q<Button>("MenuButton");
         HowToButton = root.Q<Button>("HowToButton");
 
-        PlayButton.clicked += PlayButtonPressed;
-        //SettingsButton.clicked += SettingsButtonPressed;
+
+
+        PlayButton.clicked += PlayGame;
+        OptionsButton.clicked += SettingsMenu;
         //HowToButton.clicked += HowToButtonPressed;
         QuitButton.clicked += ExitGame;
     }
 
-    void PlayButtonPressed()
+    void PlayGame()
     {
         SceneManager.LoadScene("Main");
     }
 
-    //void SettingsButtonPressed()
-    //{
-        
-    //}
+    void SettingsMenu()
+    {
+        Container.Clear();
+    }
 
     //void HowToButtonPressed()
     //{
 
     //}
-    
+
     void ExitGame()
     {
         Application.Quit();
